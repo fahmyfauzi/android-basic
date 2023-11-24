cde45bf8f9fc0f152007c8d9d6c040366fe510b4
Fahmy <fahmyfauzii@gmail.com>
2023-11-24 15:50:49 +0700

    uri (unique resource identifier)

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -1,29 +1,77 @@
 package com.example.androidbasics
 
+import android.app.Instrumentation.ActivityResult
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.net.toUri
 import com.example.androidbasics.ui.theme.AndroidBasicsTheme
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Membuat URI dari drawable resource di dalam aplikasi Android
+        val uri = Uri.parse("android.resource://$packageName/drawable/myimage")
+
+        // Membaca byte data dari InputStream menggunakan URI
+        val myimageByte = contentResolver.openInputStream(uri)?.use {
+            it.readBytes()
+        }
+
+        // Menampilkan ukuran file gambar dalam logcat
+        println("MyImage size: ${myimageByte?.size}")
+
+        // Menyimpan byte data ke file lokal (contoh: myimage.jpg)
+        val file = File(filesDir,"myimage.jpg")
+        FileOutputStream(file).use {
+            it.write(myimageByte)
+        }
+        // Menampilkan URI file lokal dalam logcat
+        println(file.toUri())
+
         setContent {
             AndroidBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                // Membuat launcher untuk memilih gambar dari galeri
+                val pickImage = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.GetContent(),
+                    onResult = {contentUri->
+                        // Menampilkan URI dari gambar yang dipilih dari galeri dalam logcat
+                        println(contentUri)
+                    }
+                )
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    Button(onClick = {
+                        pickImage.launch("image/*")
+                    },
+
+                        ) {
+                        Text(text = "Pick Image")
+                    }
                 }
+
+
             }
         }
     }
