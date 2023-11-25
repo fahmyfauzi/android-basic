269534949e12931eb81c2455dd201d38ed8741f9
Fahmy <fahmyfauzii@gmail.com>
2023-11-25 14:05:10 +0700

    content providers

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -1,33 +1,111 @@
 package com.example.androidbasics
 
+import android.Manifest.permission.READ_MEDIA_IMAGES
+import android.content.ContentResolver
+import android.content.ContentUris
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.ViewModel
+import coil.compose.AsyncImage
 import com.example.androidbasics.ui.theme.AndroidBasicsTheme
+import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<ImageViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(READ_MEDIA_IMAGES),
+            0
+        )
+
+        val projection = arrayOf(
+            MediaStore.Images.Media._ID,
+            MediaStore.Images.Media.DISPLAY_NAME
+        )
+        val milisYesterday = Calendar.getInstance().apply {
+            add(Calendar.DAY_OF_YEAR, -1)
+        }.timeInMillis
+
+        val selection = "${MediaStore.Images.Media.DATE_TAKEN} >= ?"
+        val selectionArgs = arrayOf(milisYesterday.toString())
+
+        val sortOrder = "${MediaStore.Images.Media.DATE_TAKEN} DESC"
+
+        contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            selectionArgs,
+            sortOrder
+        )?.use {cursor ->
+            val idColumn = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val nameColumn = cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME)
+
+            val images = mutableListOf<Image>()
+
+            while(cursor.moveToNext()){
+                val id = cursor.getLong(idColumn)
+                val name = cursor.getString(nameColumn)
+                val uri = ContentUris.withAppendedId(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    id
+                )
+                images.add(Image(id,name,uri))
+            }
+            viewModel.updateImages(images)
+        }
+
+
         setContent {
             AndroidBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize()
+                ){
+                    items(viewModel.images){image->
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            AsyncImage(model = image.uri, contentDescription = null)
+                            Text(text = image.name)
+                        }
+                        
+                    }
                 }
             }
         }
     }
 }
+
+data class Image(
+    val id:Long,
+    val name:String,
+    val uri:Uri
+)
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
